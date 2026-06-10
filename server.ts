734effe6cdfbf6dfc2dd9a73637efe287c1fbb9c
pankaj-getsetyo <pankaj@getsetyo.com>
2026-06-10 09:27:44 +0530

    cloud deployemnet issue

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ dotenv.config({ path: '.env.local' });
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
 app.use(express.json());
 
